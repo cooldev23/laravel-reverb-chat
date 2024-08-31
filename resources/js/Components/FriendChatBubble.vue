5a43d6bc4c83c18f676fd1f670f496fd847ee128
Sean Cordill <scordill23@gmail.com>
2024-08-31 00:15:10 -0600

    Minor styling updates

--- resources/js/Components/FriendChatBubble.vue
+++ resources/js/Components/FriendChatBubble.vue
@@ -5,5 +5,5 @@ const props = defineProps({
 </script>
 
 <template>
-  <p class="p-2 mr-auto text-white bg-green-500 rounded-lg min-w-48 max-w-72">{{ message.text }}</p>
+  <p class="p-2 mr-auto text-white bg-green-500 rounded-lg min-w-48 max-w-72 shadow-sm">{{ message.text }}</p>
 </template>
